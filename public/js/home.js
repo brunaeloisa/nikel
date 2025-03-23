11c67c7de188a6b2f68e3cbafcd93a02fe0b975b
bruna <brunaeloisa7@gmail.com>
2025-03-23 13:24:45 -0300

    add new transaction feature

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,5 @@
 const logOutButton = document.getElementById('logout-button');
+const transactionForm = document.getElementById('transaction-form');
 const transactionModal = new bootstrap.Modal('#transaction-modal');
 const session = localStorage.getItem('session');
 let logged = sessionStorage.getItem('logged');
@@ -7,6 +8,28 @@ let data = { transactions: [] };
 checkLogged();
 
 logOutButton.addEventListener('click', logout);
+
+// Adicionar lançamento
+transactionForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const value = parseFloat(document.getElementById('value-input').value);
+  const description = document.getElementById('description-input').value;
+  const date = document.getElementById('date-input').value;
+  const type = document.querySelector('input[name="type-input"]:checked').value;
+  
+  data.transactions.unshift({
+    value,
+    type,
+    description,
+    date
+  });
+
+  saveData(data);
+  e.target.reset();
+  transactionModal.hide();
+  alert('Lançamento adicionado com sucesso!');
+});
 
 function checkLogged() {
   if (session) {
@@ -29,4 +52,8 @@ function logout() {
   sessionStorage.removeItem('logged');
   localStorage.removeItem('session');
   window.location.href = './index.html';
+}
+
+function saveData(data) {
+  localStorage.setItem(data.login, JSON.stringify(data));
 }
